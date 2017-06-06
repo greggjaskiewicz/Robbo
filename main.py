fc311c9abf73ae321c1a910fa75af8c99e1d8b1d
Kondoktor <worms3d4@tlen.pl>
2017-06-07 00:11:55 +0200

    V.1 Release!
    Main mechanics done.
    One thing that is bothering me,
    is that the ‘.’ is drawing beneath the barrels, but for now i don’t
    know how to fix it.

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame
 import math
 
 ####constants####
-WIDTH = 800 # 800 adjusted for map 25x25: 373
-HEIGHT = 600 # 600 adjusted for map 25x25  405
+WIDTH = 373 # 800 adjusted for map 25x25: 373
+HEIGHT = 405 # 600 adjusted for map 25x25: 405
 #colors#
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (192, 192, 192)
 BROWN = (102, 51, 0)
 ROBBO_GREEN = (0, 150, 0)
+RED = (255, 0, 0)
 ########
 #chars#
 char_PLAYER = '@'
@@ -28,7 +29,7 @@ MAX_NUMBER_LEVELS = 2
 
 #classes#
 # TODO MAKE A WORLD CLASS, WHICH WILL BE GOING TO HAVE ALL OF DATA
-
+# TODO MAKE A CLASS FOR EVERY OBJECT
 
 class PlayerClass: # will be the player and the barrel
 
@@ -39,7 +40,7 @@ class PlayerClass: # will be the player and the barrel
         self.screws = screws
 
     def move(self, dx, dy):
-        if not (self.x + dx, self.y + dy) in WALL_LIST:
+        if not (self.x + dx, self.y + dy) in WALL_LIST: # and Barrel.x .y (BARREL_INSTANCES(PUSHABLE_LIST))
             self.x += dx
             self.y += dy
 
@@ -56,28 +57,54 @@ class PlayerClass: # will be the player and the barrel
     def draw(self, icon):
         WINDOW.blit(icon, (self.x, self.y))
 
-    def push(self, pushable_list, bin):
+    def push(self, pushable_list, dx, dy):
+        #CHANGED_BARRELS_LIST.append((pushable_list[0][0].x, pushable_list[0][0].y))
+        global CHANGING_PUSHABLE_LIST
+        CHANGING_PUSHABLE_LIST = unpack_INSTANCES(pushable_list)
         for p1 in pushable_list:
+            dir_x = p1[0].x + dx
+            dir_y = p1[0].y + dy
             if distance(p1[0].x, p1[0].y, self.x, self.y) < 2:
-                print p1[0].x, p1[0].y
-                if not (p1[0].x + char_SIZE) in BARREL_INSTANCES:
-                    p1[0].x += char_SIZE
-                #bin.append((p1[0].x, p1[0].y))
+                if not (dir_x, dir_y) in WALL_LIST:
+                    if (dir_x, dir_y) in CHANGING_PUSHABLE_LIST:
+                        self.move(-dx, -dy)
+                    else:
+                        p1[0].x += dx
+                        p1[0].y += dy
+                else:
+                    self.move(-dx, -dy)
+
+            #if distance(p1[0].x, p1[0].y, self.x, self.y) < 2:
+            #    p1[0].x += 15
+
+        #for p1 in pushable_list:
+        #    for p2 in pushable_list:
+        #        if distance(p1[0].x, p1[0].y, self.x, self.y) < 2:
+        #            if distance(p1[0].x, p1[0].y, p2[0].x, p2[0].y) > 2:
+        #                p1[0].x += 15
+
+                #if distance(p1[0].x, p1[0].y, p1[0].x, p1[0].y) < 2:
+                #    print 'd'
+            #if distance(p1[0].x, p1[0].y, p1[0].x, p1[0].y) < 2:
+            #    if distance(p1[0].x, p1[0].y, self.x, self.y) > 2:
+            #        if (p1[0].x + char_SIZE, p1[0].y + char_SIZE) in SPACE_LIST:
+            #            print p1[0].x, p1[0].y
+            #            bin.append((p1[0].x, p[0].y))
+            #            p1[0].x += 15
 
 
 class BarrelClass:
 
+    global barrels
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-    def move(self, dx, dy):
-        if not (self.x + dx, self.y + dy) in WALL_LIST or BARREL_INSTANCES:
-            self.x += dx
-            self.y += dy
+        global barrels
+        barrels = [self.x, self.y]
 
     def draw(self, icon):
-        pass
+        WINDOW.blit(icon, (self.x, self.y))
 
 #GAME#
 
@@ -98,22 +125,30 @@ def game_loop():
                     exit(0)
                 elif event.key == pygame.K_UP:
                     Player.move(0, -15)
+                    Player.push(BARREL_INSTANCES, 0, -15)
                 elif event.key == pygame.K_DOWN:
                     Player.move(0, 15)
+                    Player.push(BARREL_INSTANCES, 0, 15)
                 elif event.key == pygame.K_LEFT:
                     Player.move(-15, 0)
+                    Player.push(BARREL_INSTANCES, -15, 0)
                 elif event.key == pygame.K_RIGHT:
                     Player.move(15, 0)
+                    Player.push(BARREL_INSTANCES, 15, 0)
                 elif event.key == pygame.K_d:
                     #descend = True # change map descent not present in this version
-                    # TODO MAKE MULTIPLE LEVELS
+                    # TODO MAKE MULTIPLE LEVELS, WITH STAIRS JUST FOR THE SAKE OF IT
                     pass
 
-        game_logic()
+        if not game_won:
+            game_logic()
+        else:
+            victory_screen()
 
 def initialize_game():
 
     global WINDOW
+    global FONT
     global PLAYER
     global WALL
     global SPACE
@@ -122,7 +157,8 @@ def initialize_game():
     global STAIR_UP
     global SCREW
     global BARREL
-    global TEXT
+    global STATS
+    global VERSION
 
     pygame.init()
     pygame.font.init()
@@ -136,7 +172,8 @@ def initialize_game():
     STAIR_UP = FONT.render(char_STAIR_UP, 0, WHITE)
     SCREW = FONT.render(char_SCREW, 0, GREY)
     BARREL = FONT.render(char_BARREL, 0, BROWN)
-    TEXT = FONT.render('HP:  Screws:  Level: ', 0, WHITE)
+    STATS = FONT.render("HP:{0} Screws:{1} Level:{2}".format(Player.hp, Player.screws, current_level_number), 0, WHITE)
+    VERSION = FONT.render("v.1.0", 0, RED)
 
     WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -154,6 +191,7 @@ def check_if_descended():
         return True
 
 def getcurrent_map():
+    global current_level_number
     global current_level
     current_level = []
     current_level_number = 1
@@ -201,7 +239,11 @@ def get_objs():
     global BARREL_LIST
     global Barrel
     global BARREL_INSTANCES
+    global CHANGING_PUSHABLE_LIST
+    global SCREWS
 
+    SCREWS = 0
+    CHANGING_PUSHABLE_LIST = []
     EMPTY_LIST = []
     WALL_LIST = []
     SCREW_LIST = []
@@ -254,6 +296,7 @@ def get_objs():
 
             elif char == char_SCREW:
                 (scr_x, scr_y) = (char_x, char_y) # starting position
+                SCREWS += 1
                 SCREW_LIST.append((scr_x, scr_y))
 
             elif char == char_EMPTY:
@@ -262,10 +305,12 @@ def get_objs():
 
             elif char == char_BARREL:
                 (b_x, b_y) = (char_x, char_y) # starting position of the barrels
-                for b in char:
-                    Barrel = BarrelClass(b_x, b_y)
-                    BARREL_INSTANCES.append([Barrel])
-                #BARREL_LIST.append([Barrel.x, Barrel.y])
+                #for b in char:
+                Barrel = BarrelClass(b_x, b_y)
+                BARREL_INSTANCES.append([Barrel])
+                BARREL_LIST.append((b_x, b_y))
+                EMPTY_LIST.append((b_x, b_y))
+                #print BARREL_INSTANCES[0][0].x, BARREL_INSTANCES[0][0].y
 
 
 
@@ -318,12 +363,12 @@ def draw_map():
         if distance(Player.x, Player.y, sd[0], sd[1]) > 2:
             WINDOW.blit(SPACE, (sd[0], sd[1]))
 
-        if distance(sd[0], sd[1], BARREL_INSTANCES[0][0].x, BARREL_INSTANCES[0][0].y) > 2:
-            WINDOW.blit(BARREL, (BARREL_INSTANCES[0][0].x, BARREL_INSTANCES[0][0].y))
-            # i think i have to do a calculation with the changed barre_instances.x, .y
+        #if distance(sd[0], sd[1], BARREL_INSTANCES[0][0].x, BARREL_INSTANCES[0][0].y) > 2:
+        #    WINDOW.blit(BARREL, (BARREL_INSTANCES[0][0].x, BARREL_INSTANCES[0][0].y))
+            # i think i have to do a calculation with the changed barrel_instances.x, .y
 
     for b in BARREL_INSTANCES:
-        if distance(Player.x, Player.y, b[0].x, b[0].y) > 2: # it is viewing the other list
+        if distance(Player.x, Player.y, b[0].x, b[0].y) > 2:
             WINDOW.blit(BARREL, (b[0].x, b[0].y))
 
     if level_completed:
@@ -336,14 +381,31 @@ def draw_map():
             pass
 
 
+def draw_stats():
+    STATS = FONT.render("HP:{0} Screws:{1} Level:{2}".format(Player.hp, Player.screws, current_level_number), 0, WHITE)
+    WINDOW.blit(STATS, ((25 * char_SIZE - char_SIZE * 25), (25 * char_SIZE + char_SIZE)))
+
+def draw_version():
+    WINDOW.blit(VERSION, (WIDTH - char_SIZE * 5, HEIGHT - char_SIZE))
+
 def distance(ax, ay, bx, by):
     aa = ax - bx
     bb = ay - by
     c = math.sqrt((aa ** 2) + (bb ** 2))
     return c
 
+def unpack_INSTANCES(package):
+    list = []
+    for l in package:
+        list.append((l[0].x, l[0].y))
+    return list
+
+def check_for_win():
+    if Player.screws == SCREWS:
+        return True
 
 def game_logic():
+    global game_won
     # what is actually done in steps for the game to be
 
     # clear
@@ -359,14 +421,15 @@ def game_logic():
     # check for gather
     Player.take(SCREW_LIST, EMPTY_LIST)
 
-    # check for push
-    Player.push(BARREL_INSTANCES, EMPTY_LIST)
-
     # draw the map
     draw_map()
 
     # draw stats
-    WINDOW.blit(TEXT, ((25 * char_SIZE - char_SIZE * 25), (25 * char_SIZE + char_SIZE)))
+    draw_stats()
+
+    # check for win
+
+    game_won = check_for_win()
 
     # update screen
     pygame.display.flip()
@@ -374,14 +437,27 @@ def game_logic():
 
 #run#
 
+def victory_screen():
+    WINDOW.fill(BLACK)
+    WON = FONT.render('YOU HAVE GATHERED', 0, WHITE)
+    WON2 = FONT.render('ALL THE SCREWES :DDD', 0 , WHITE)
+    WINDOW.blit(WON, (WIDTH / 2 - 130, HEIGHT / 2 - 15))
+    WINDOW.blit(WON2, (WIDTH / 2 - 150, HEIGHT / 2))
+    draw_version()
+    pygame.display.flip()
+
 if __name__ == '__main__':
+    game_won = False
     initialize_game()
     getcurrent_map()
     get_objs()
-    print BARREL_INSTANCES
-    print BARREL_INSTANCES[4][0].x # ACCESSING INSTANCES
-    print BARREL_INSTANCES[4][0].y
+    #print BARREL_INSTANCES
+    #print BARREL_INSTANCES[4][0].y
+    #for p in BARREL_INSTANCES:
+    #    print p[0].y
     #BARREL_INSTANCES[0][0].x += 1
     #BARREL_INSTANCES[0][0].y += 1
+    #print BARREL_LIST
     #draw_map()
+    #print barrels
     game_loop()
